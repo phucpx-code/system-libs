b96e3dd967f8241c5096898481b1823417d8e471
phamxuanphuc <phuc.pxp@gmail.com>
2026-05-24 22:45:43 +0700

    Add React and ReactDOM entries to libs.json and create corresponding index files

---
+++ src/react-dom/index.js
@@ -0,0 +1,7 @@
+export {
+  __DOM_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE,
+  createPortal, flushSync, preconnect, prefetchDNS, preinit, preinitModule,
+  preload, preloadModule, requestFormReset, unstable_batchedUpdates,
+  useFormState, useFormStatus, version
+} from 'react-dom';
+export { default } from 'react-dom';
